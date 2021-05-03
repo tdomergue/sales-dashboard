ff2559400beb7883c618bbca6a96d7b7cd11040a
Thibaud Domergue <thibaud.domergue@hei.fr>
2021-05-03 13:26:15 +0200

    prepare seeds to import csv in the db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'csv'
+
+puts "Parsing the data.csv to inject it in DB, you can go grab a coffee (even in Brazil) and come back..."
+puts "Sales imported :"
+
+Sale.destroy_all
+
+csv_options = { col_sep: ',', quote_char: '"', headers: :first_row }
+filepath = 'db/memory-tech-challenge-data.csv'
+
+i = 0
+CSV.foreach(filepath, csv_options) do |row|
+  Sale.create!({
+    date: Date.parse(row['date']),
+    order: row['order_id'].to_i,
+    customer: row['customer_id'].to_i,
+    country: row['country'],
+    price: row['quantity'].to_i * row['unit_price'].to_f
+  })
+  i += 1
+  puts "#{i}       / 406829"
+end
+
+puts "#{Sale.count} sales well and slowly imported!"
